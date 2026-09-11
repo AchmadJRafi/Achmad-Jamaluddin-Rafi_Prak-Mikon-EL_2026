d40ac6c3a83dd42b7870a4eb647941fd57289ccc
AchmadJRafi <achmadjamaluddinrafi@mail.ugm.ac.id>
2026-09-11 17:14:15 +0700

    pertemuan 2 tugas 2

--- Pertemuan_2/src/main.cpp
+++ Pertemuan_2/src/main.cpp
@@ -1,14 +1,27 @@
 #include <Arduino.h>
 
-const int ledPin = 16;
+const int buttonPin = 14;
+const int ledPin1 = 27;
+const int ledPin2 = 26;
+const int ledPin3 = 25;
 
 void setup() {
-  pinMode(16, OUTPUT);
+  pinMode(14, INPUT_PULLDOWN);
+  pinMode(27, OUTPUT);
+  pinMode(26, OUTPUT);
+  pinMode(25, OUTPUT);
 }
 
 void loop() {
-  delay(3000);
-  digitalWrite(16, HIGH);
-  delay(3000);
-  digitalWrite(16, LOW);
+  if (digitalRead(buttonPin) == HIGH)
+  {
+    digitalWrite(27, HIGH);
+    digitalWrite(26, HIGH);
+    digitalWrite(25, HIGH);
+  }
+  else{
+    digitalWrite(27, LOW);
+    digitalWrite(26, LOW);
+    digitalWrite(25, LOW);
+  }
 }
